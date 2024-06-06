3630903fec9e2f7456a53745af6b93d25b995626
Priccila Lucem <priccilalucem@gmail.com>
2024-06-06 09:51:16 -0300

    feat/category

---
+++ store/src/main/java/com/example/store/entities/CategoryEntity.java
@@ -0,0 +1,22 @@
+package com.example.store.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 255)
+    private String nome;
+
+    @Column(name="descricao", nullable = false, columnDefinition = "TEXT")
+    private String descricao;
+}
