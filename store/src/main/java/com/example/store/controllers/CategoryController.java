e0f70ed5d8c9f18fbe9144b7e492380773face8a
Priccila Lucem <priccilalucem@gmail.com>
2024-06-06 19:27:24 -0300

    feat/documentation

--- store/src/main/java/com/example/store/controllers/CategoryController.java
+++ store/src/main/java/com/example/store/controllers/CategoryController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.store.entities.CategoryEntity;
 import com.example.store.service.CategoryService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,32 +31,63 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/category")
+@Tag(name = "Categories", description = "Categories crud")
 public class CategoryController {
     
     @Autowired
     private CategoryService categoryService;
 
+    @Operation(
+        responses = @ApiResponse(description = "OK", responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CategoryEntity.class)))
+    ))
     @GetMapping()
     public ResponseEntity<List<CategoryEntity>> getAllCategories() {
         return ResponseEntity.ok().body(categoryService.filterAllCategoriesService());
     }
 
+    @Operation(
+        responses = { 
+            @ApiResponse(description = "OK", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CategoryEntity.class))),
+            @ApiResponse(description = "Not found", responseCode = "404", content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+        }
+    )
     @GetMapping("/search")
     public ResponseEntity<CategoryEntity> getCategoryByName(@RequestParam String nome) {
         return ResponseEntity.ok().body(categoryService.filterByCategoryNameService(nome));
     }
     
+    @Operation(
+        responses = {
+            @ApiResponse(description = "Created", responseCode = "201", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CategoryEntity.class))),
+            @ApiResponse(description = "Bad request", responseCode = "400", content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+        }
+    )
     @PostMapping()
     public ResponseEntity<CategoryEntity> postCategory(@RequestBody CategoryEntity entity) {
         return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.postCategoryService(entity));
     }
 
+    @Operation(
+        responses = {
+            @ApiResponse(description = "Created", responseCode = "202", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CategoryEntity.class))),
+            @ApiResponse(description = "Bad request", responseCode = "400", content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+            @ApiResponse(description = "Not found", responseCode = "404", content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+        
+        }
+    )
     @PutMapping("/{id}")
     public ResponseEntity<CategoryEntity> putCategoryEntity(@PathVariable Long id, @RequestBody CategoryEntity entity) throws BadRequestException {
         entity.setId(id);
         return ResponseEntity.accepted().body(categoryService.putCategoryService(entity));
     }
 
+
+    @Operation(
+        responses = {
+            @ApiResponse(description = "No body", responseCode = "204"),
+            @ApiResponse(description = "Not found", responseCode = "404", content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+        }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteCategoryController(@PathVariable Long id){
         categoryService.deleteCategoryService(id);
--- store/src/main/java/com/example/store/controllers/ProductController.java
+++ store/src/main/java/com/example/store/controllers/ProductController.java
@@ -10,6 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.store.dto.ProductCategoryDTO;
 import com.example.store.entities.ProductEntity;
 import com.example.store.service.ProductService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,31 +31,65 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/product")
+@Tag(name = "Products", description = "Products crud")
 public class ProductController {
     
     @Autowired
     private ProductService productService;
 
+
+    @Operation(
+        responses = {
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+            @ApiResponse(description = "Created", responseCode = "201", content = @Content(schema = @Schema(implementation = ProductEntity.class)))
+        }
+    )
     @PostMapping()
     public ResponseEntity<ProductEntity> postProductController(@RequestBody ProductCategoryDTO entity) {        
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.postProductService(entity));
     }
-    
+
+    @Operation(
+        responses = {
+            @ApiResponse(description = "Ok", responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ProductEntity.class))))
+        }
+    )
     @GetMapping()
     public  ResponseEntity<List<ProductEntity>>  getAllProductsController(@RequestParam String param) {
         return ResponseEntity.ok().body(productService.filterAllProductsService());
     }
     
+    @Operation(
+        responses = {
+            @ApiResponse(description = "Not found", responseCode = "404", content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+            @ApiResponse(description = "Ok", responseCode = "200", content = @Content(schema = @Schema(implementation = ProductEntity.class)))
+        }
+    )
     @GetMapping("/{product_id}")
     public ResponseEntity<ProductEntity> getOneProductController(@PathVariable(name = "product_id") Long id) {
         return ResponseEntity.ok().body(productService.filterByProductIdService(id));
     }
     
+    @Operation(
+        responses = {
+            @ApiResponse(description = "Not found", responseCode = "404", content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+            @ApiResponse(description = "Accepted", responseCode = "202", content = @Content(schema = @Schema(implementation = ProductEntity.class))),
+
+        } 
+    )
     @PutMapping("/{id}")
     public ResponseEntity<ProductEntity> putProductController(@PathVariable Long id, @RequestBody ProductEntity entity) throws BadRequestException {
         entity.setId(id);
         return ResponseEntity.accepted().body(productService.putProductService(entity));
     }
+
+    @Operation(
+        responses = {
+            @ApiResponse(description = "No body", responseCode = "204"),
+            @ApiResponse(description = "Not found", responseCode = "404", content = @Content(schema = @Schema(implementation = GlobalExceptionHandler.ErrorResponse.class))),
+        }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteProductController(@PathVariable Long id){
         productService.deleteProductsService(id);
